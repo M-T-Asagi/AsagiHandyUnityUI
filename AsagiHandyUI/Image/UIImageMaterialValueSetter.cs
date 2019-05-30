5fb5b3a8d2087bc65e7c2ce74fcf3789668a67ea
VC00746 <aterada@M-148noMac-mini.local>
2019-05-30 19:31:40 +0900

    UIImageValueSetterを作成

---
+++ AsagiHandyUI/Image/UIImageMaterialValueSetter.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIImageMaterialValueSetter : MonoBehaviour
+{
+    [SerializeField]
+    GameObject target = null;
+
+    [SerializeField]
+    string targetMaterialValueName = "";
+
+    //TODO 複数型に対応
+    [SerializeField]
+    float val = 0;
+
+    RawImage targetRaw;
+    Image targetImage;
+
+    float lastValue;
+
+    Material mat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetRaw = target.GetComponent<RawImage>();
+        if (targetRaw != null)
+        {
+            mat = targetRaw.material;
+        }
+        else
+        {
+            targetImage = target.GetComponent<Image>();
+            mat = targetImage.material;
+        }
+
+        lastValue = val;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(lastValue != val)
+        {
+            mat.SetFloat(targetMaterialValueName, val);
+            lastValue = val;
+        }
+    }
+}
