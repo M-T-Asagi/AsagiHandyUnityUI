3563b6c835f37dd33b0c58ab91fa749f58c395bd
VC00746 <aterada@M-148noMac-mini.local>
2019-07-26 11:16:01 +0900

    デバッグログのスクロール位置が変わって読みにくいので修正

---
+++ AsagiHandyUI/DebugLogViewer/Scripts/DebugLogScrollerPositionFitter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace AsagiHandy.UI.DebugLogViewer
+{
+    public class DebugLogScrollerPositionFitter : MonoBehaviour
+    {
+        [SerializeField]
+        ScrollRect scrollRect = null;
+
+        float lastPosition = 0f;
+
+        public void positionUpdate(Vector2 position)
+        {
+            lastPosition = position.y;
+        }
+
+        public void PositionFitting()
+        {
+            if (lastPosition <= 0.001f || lastPosition >= 0.999f)
+            {
+                Vector2 newPos = scrollRect.normalizedPosition;
+                newPos.y = Mathf.Round(newPos.y);
+                scrollRect.normalizedPosition = newPos;
+            }
+        }
+    }
+}
